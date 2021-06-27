36a450ed4f9f487ecb17feb9be206ae92d795f52
Angad Singh <mail@angad.dev>
2021-06-27 08:08:34 +0530

    feat: download and save pdf

--- script.js
+++ script.js
@@ -1,121 +1,147 @@
 const puppeteer = require("puppeteer");
 const inquirer = require("inquirer");
 const htmlEntities = require("html-entities");
+const { exec: execCallback } = require("child_process");
+const { promisify } = require("util");
+const exec = promisify(execCallback);
 
 const URL = (path) => `https://ncert.nic.in/${path}`;
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(URL("textbook.php"), {
-    waitUntil: "networkidle0",
-  });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(URL("textbook.php"), {
+      waitUntil: "networkidle0",
+    });
 
-  const classes = await page.evaluate(() => {
-    return [...document.querySelectorAll('select[name="tclass"] option')]
-      .map((x) => [x.value, x.innerHTML])
-      .filter((x) => !!x[0] || !!x[1])
-      .slice(1);
-  });
+    const classes = await page.evaluate(() => {
+      return [...document.querySelectorAll('select[name="tclass"] option')]
+        .map((x) => [x.value, x.innerHTML])
+        .filter((x) => !!x[0] || !!x[1])
+        .slice(1);
+    });
 
-  const selClass = await inquirer.prompt([
-    {
-      type: "list",
-      name: "class",
-      message: "Class",
-      choices: classes.map((x) => htmlEntities.decode(x[1])),
-    },
-  ]);
+    const selClass = await inquirer.prompt([
+      {
+        type: "list",
+        name: "class",
+        message: "Class",
+        choices: classes.map((x) => htmlEntities.decode(x[1])),
+      },
+    ]);
 
-  const selClassOption = classes.find(
-    (x) => x[1] === htmlEntities.encode(selClass.class)
-  )[0];
+    const selClassOption = classes.find(
+      (x) => x[1] === htmlEntities.encode(selClass.class)
+    )[0];
 
-  await page.select('select[name="tclass"]', String(selClassOption));
+    await page.select('select[name="tclass"]', String(selClassOption));
 
-  const subjects = await page.evaluate(() => {
-    return [...document.querySelectorAll('select[name="tsubject"] option')]
-      .map((x) => [x.value, x.innerHTML])
-      .filter((x) => !!x[0] || !!x[1])
-      .slice(1);
-  });
+    const subjects = await page.evaluate(() => {
+      return [...document.querySelectorAll('select[name="tsubject"] option')]
+        .map((x) => [x.value, x.innerHTML])
+        .filter((x) => !!x[0] || !!x[1])
+        .slice(1);
+    });
 
-  const selSubject = await inquirer.prompt([
-    {
-      type: "list",
-      name: "subject",
-      message: "Subject",
-      choices: subjects.map((x) => htmlEntities.decode(x[1])),
-    },
-  ]);
+    const selSubject = await inquirer.prompt([
+      {
+        type: "list",
+        name: "subject",
+        message: "Subject",
+        choices: subjects.map((x) => htmlEntities.decode(x[1])),
+      },
+    ]);
 
-  const selSubjectOption = subjects.find(
-    (x) => x[1] === htmlEntities.encode(selSubject.subject)
-  )[0];
+    const selSubjectOption = subjects.find(
+      (x) => x[1] === htmlEntities.encode(selSubject.subject)
+    )[0];
 
-  await page.select('select[name="tsubject"]', String(selSubjectOption));
+    await page.select('select[name="tsubject"]', String(selSubjectOption));
 
-  const textbooks = await page.evaluate(() => {
-    return [...document.querySelectorAll('select[name="tbook"] option')]
-      .map((x) => [x.value, x.innerHTML])
-      .filter((x) => !!x[0] || !!x[1])
-      .slice(1);
-  });
+    const textbooks = await page.evaluate(() => {
+      return [...document.querySelectorAll('select[name="tbook"] option')]
+        .map((x) => [x.value, x.innerHTML])
+        .filter((x) => !!x[0] || !!x[1])
+        .slice(1);
+    });
 
-  const { textbook } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "textbook",
-      message: "Textbook",
-      choices: textbooks.map((x) => htmlEntities.decode(x[1])),
-    },
-  ]);
+    const { textbook } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "textbook",
+        message: "Textbook",
+        choices: textbooks.map((x) => htmlEntities.decode(x[1])),
+      },
+    ]);
 
-  const textbookLink = textbooks.find(
-    (x) => x[1] === htmlEntities.encode(textbook)
-  )[0];
+    const textbookLink = textbooks.find(
+      (x) => x[1] === htmlEntities.encode(textbook)
+    )[0];
 
-  await page.goto(URL(textbookLink), {
-    waitUntil: "networkidle0",
-  });
+    await page.goto(URL(textbookLink), {
+      waitUntil: "networkidle0",
+    });
 
-  const toc = await page.evaluate(() => {
-    return [
-      ...document
-        .querySelector(
-          // Yikes
-          "#Layer1 > center > p > table:nth-child(2) > tbody > tr > td:nth-child(1) > div > table > tbody"
-        )
-        .querySelectorAll("tbody > tr > td > table > tbody > tr"),
-    ]
-      .map((x) => [...x.querySelectorAll("tr > td")])
-      .map(([title, link]) => [
-        link.querySelector("a").getAttribute("href"),
-        title.innerText,
-      ])
-      .filter((x) => !!x[0] || !!x[1]);
-  });
+    const toc = await page.evaluate(() => {
+      return [
+        ...document
+          .querySelector(
+            // Yikes
+            "#Layer1 > center > p > table:nth-child(2) > tbody > tr > td:nth-child(1) > div > table > tbody"
+          )
+          .querySelectorAll("tbody > tr > td > table > tbody > tr"),
+      ]
+        .map((x) => [...x.querySelectorAll("tr > td")])
+        .map(([title, link]) => [
+          link.querySelector("a").getAttribute("href"),
+          title.innerText,
+        ])
+        .filter((x) => !!x[0] || !!x[1]);
+    });
 
-  const { section } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "section",
-      message: "Section / Chapter",
-      choices: toc.map((x) => htmlEntities.decode(x[1])),
-    },
-  ]);
+    const { section } = await inquirer.prompt([
+      {
+        type: "list",
+        name: "section",
+        message: "Section / Chapter",
+        choices: toc.map((x) => htmlEntities.decode(x[1])),
+      },
+    ]);
 
-  const sectionLink = toc.find((x) => x[1] === htmlEntities.encode(section))[0];
+    const sectionLink = toc.find(
+      (x) => x[1] === htmlEntities.encode(section)
+    )[0];
 
-  await page.goto(URL(sectionLink), {
-    waitUntil: "networkidle0",
-  });
+    await page.goto(URL(sectionLink), {
+      waitUntil: "networkidle0",
+    });
 
-  const PDFLink = await page.evaluate(() => {
-    return document.querySelector("iframe").getAttribute("src");
-  });
+    const PDFLink = URL(
+      await page.evaluate(() => {
+        return document.querySelector("iframe").getAttribute("src");
+      })
+    );
 
-  console.log({ PDFLink });
+    const filenameDef = PDFLink.split("/")[PDFLink.split("/").length - 1];
+    const { filename } = await inquirer.prompt([
+      {
+        type: "input",
+        name: "filename",
+        message: "Save as",
+        default: filenameDef,
+      },
+    ]);
 
-  await browser.close();
+    const { stderr } = await exec(`wget ${PDFLink} -O ${filename}.pdf`);
+
+    if (!!stderr) {
+      console.log("WGET Error");
+      console.log(stderr);
+    }
+
+    await browser.close();
+  } catch (e) {
+    console.error(e);
+  }
 })();
