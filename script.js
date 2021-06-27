b48f99cbebd0d19ab1b8e3d9ad9c18ed748c7082
Angad Singh <mail@angad.dev>
2021-06-27 07:55:53 +0530

    feat: get link to section

--- script.js
+++ script.js
@@ -2,21 +2,114 @@ const puppeteer = require("puppeteer");
 const inquirer = require("inquirer");
 const htmlEntities = require("html-entities");
 
+const URL = (path) => `https://ncert.nic.in/${path}`;
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://ncert.nic.in/textbook.php", {
+  await page.goto(URL("textbook.php"), {
     waitUntil: "networkidle0",
   });
-  await page.screenshot({ path: "example.png" });
 
   const classes = await page.evaluate(() => {
     return [...document.querySelectorAll('select[name="tclass"] option')]
       .map((x) => [x.value, x.innerHTML])
+      .filter((x) => !!x[0] || !!x[1])
       .slice(1);
   });
 
-  console.log({ classes });
+  const selClass = await inquirer.prompt([
+    {
+      type: "list",
+      name: "class",
+      message: "Class",
+      choices: classes.map((x) => htmlEntities.decode(x[1])),
+    },
+  ]);
+
+  const selClassOption = classes.find(
+    (x) => x[1] === htmlEntities.encode(selClass.class)
+  )[0];
+
+  await page.select('select[name="tclass"]', String(selClassOption));
+
+  const subjects = await page.evaluate(() => {
+    return [...document.querySelectorAll('select[name="tsubject"] option')]
+      .map((x) => [x.value, x.innerHTML])
+      .filter((x) => !!x[0] || !!x[1])
+      .slice(1);
+  });
+
+  const selSubject = await inquirer.prompt([
+    {
+      type: "list",
+      name: "subject",
+      message: "Subject",
+      choices: subjects.map((x) => htmlEntities.decode(x[1])),
+    },
+  ]);
+
+  const selSubjectOption = subjects.find(
+    (x) => x[1] === htmlEntities.encode(selSubject.subject)
+  )[0];
+
+  await page.select('select[name="tsubject"]', String(selSubjectOption));
+
+  const textbooks = await page.evaluate(() => {
+    return [...document.querySelectorAll('select[name="tbook"] option')]
+      .map((x) => [x.value, x.innerHTML])
+      .filter((x) => !!x[0] || !!x[1])
+      .slice(1);
+  });
+
+  const { textbook } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "textbook",
+      message: "Textbook",
+      choices: textbooks.map((x) => htmlEntities.decode(x[1])),
+    },
+  ]);
+
+  const textbookLink = textbooks.find(
+    (x) => x[1] === htmlEntities.encode(textbook)
+  )[0];
+
+  await page.goto(URL(textbookLink), {
+    waitUntil: "networkidle0",
+  });
+
+  const toc = await page.evaluate(() => {
+    return [
+      ...document
+        .querySelector(
+          // Yikes
+          "#Layer1 > center > p > table:nth-child(2) > tbody > tr > td:nth-child(1) > div > table > tbody"
+        )
+        .querySelectorAll("tbody > tr > td > table > tbody > tr"),
+    ]
+      .map((x) => [...x.querySelectorAll("tr > td")])
+      .map(([title, link]) => [
+        link.querySelector("a").getAttribute("href"),
+        title.innerText,
+      ])
+      .filter((x) => !!x[0] || !!x[1]);
+  });
+
+  const { section } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "section",
+      message: "Section / Chapter",
+      choices: toc.map((x) => htmlEntities.decode(x[1])),
+    },
+  ]);
+
+  const sectionLink = toc.find((x) => x[1] === htmlEntities.encode(section))[0];
+
+  console.log(URL(sectionLink));
+
+  // console.log({ textbookLink });
 
   await browser.close();
 })();
